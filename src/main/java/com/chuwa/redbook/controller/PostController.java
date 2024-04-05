6f7348faa5339c2d7e5bc6908db7fd0837678103
ty2806 <ty2806.alpha@gmail.com>
2024-04-04 20:01:06 -0400

    Added RUD for posts

--- src/main/java/com/chuwa/redbook/controller/PostController.java
+++ src/main/java/com/chuwa/redbook/controller/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/posts")
 public class PostController {
@@ -19,5 +21,25 @@ public class PostController {
         PostDto response = postService.createPost(postDto);
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<PostDto> getAllPosts() {
+        return postService.getAllPost();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name="id") long id) {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePostById(@RequestBody PostDto postDto, @PathVariable(name="id") long id) {
+        return ResponseEntity.ok(postService.updatePost(postDto, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePostById(@PathVariable(name="id") long id) {
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post Entity deleted successfully.", HttpStatus.OK);
     }
 }
--- src/main/java/com/chuwa/redbook/service/Impl/PostServiceImpl.java
+++ src/main/java/com/chuwa/redbook/service/Impl/PostServiceImpl.java
@@ -1,11 +1,15 @@
 package com.chuwa.redbook.service.Impl;
 
 import com.chuwa.redbook.entity.Post;
+import com.chuwa.redbook.exception.ResourceNotFoundException;
 import com.chuwa.redbook.payload.PostDto;
 import com.chuwa.redbook.service.PostService;
 import com.chuwa.redbook.dao.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostServiceImpl implements PostService {
@@ -16,19 +20,58 @@ public class PostServiceImpl implements PostService {
     @Override
     public PostDto createPost(PostDto postDto) {
 
-        Post postEntityToBeSaved = new Post();
-        postEntityToBeSaved.setTitle(postDto.getTitle());
-        postEntityToBeSaved.setDescription(postDto.getDescription());
-        postEntityToBeSaved.setContent(postDto.getContent());
+        Post postEntityToBeSaved = mapToEntity(postDto);
 
         Post postSaved = this.postRepository.save(postEntityToBeSaved);
 
-        PostDto response = new PostDto();
-        response.setId(postSaved.getId());
-        response.setTitle(postSaved.getTitle());
-        response.setDescription(postSaved.getDescription());
-        response.setContent(postSaved.getContent());
+        return mapToDto(postSaved);
+    }
 
-        return response;
+    @Override
+    public List<PostDto> getAllPost() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        return mapToDto(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatedPost = postRepository.save(post);
+        return mapToDto(updatedPost);
+    }
+
+    @Override
+    public void deletePostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        postRepository.delete(post);
+    }
+
+    private Post mapToEntity(PostDto postDto) {
+        Post postEntity = new Post();
+        postEntity.setTitle(postDto.getTitle());
+        postEntity.setDescription(postDto.getDescription());
+        postEntity.setContent(postDto.getContent());
+        return postEntity;
+    }
+
+    private PostDto mapToDto(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
     }
 }
